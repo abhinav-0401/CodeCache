ca3db49be12237432529f6a0250c4aef6aab0214
Abhinav Jha <linuxlenin04@gmail.com>
2024-03-28 17:41:13 +0530

    added antd and setup the layout for the pages

---
+++ CodeCacheFrontend/src/App.tsx
@@ -0,0 +1,68 @@
+import { ConfigProvider, Layout } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+interface ILayoutElementStyle {
+  background: string;
+  backdropFilter?: string;
+  height?: string;
+  borderRadius?: string;
+  width?: string
+}
+
+const siderStyle: ILayoutElementStyle = {
+  background: "transparent",
+  backdropFilter: "blur(2px)",
+};
+
+const mainLayoutStyle: ILayoutElementStyle = {
+  ...siderStyle,
+  height: "100vh",
+};
+
+
+const childLayoutStyle: ILayoutElementStyle = {
+  ...siderStyle,
+};
+
+const headerStyle: ILayoutElementStyle = {
+  ...siderStyle,
+  height: "10vh",
+};
+
+const footerStyle: ILayoutElementStyle = {
+  background: "#ffffff",
+};
+
+const contentStyle: ILayoutElementStyle = {
+  ...footerStyle,
+  borderRadius: "10px",
+};
+
+function App(): JSX.Element {
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          borderRadius: 10,
+          // bodyBg: 
+
+          // Alias Token
+          colorBgContainer: '#fff',
+        },
+      }}
+    >
+      <Layout hasSider style={mainLayoutStyle}>
+        <Sider width="18%" style={siderStyle}></Sider>
+        <Layout style={childLayoutStyle}>
+          <Header style={headerStyle}></Header>
+          <Content style={contentStyle}></Content>
+          <Footer style={footerStyle}></Footer>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
+  );
+}
+
+export default App;
